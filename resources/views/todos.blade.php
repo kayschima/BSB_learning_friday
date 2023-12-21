68fd95fead2e5d0173005c18f60a053b8277f9a9
Kay Markschies <kayschima@web.de>
2023-12-21 15:03:52 +0100

    add Tailwind CSS and put all todos in a table

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -6,15 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Die Todos von VD25</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-<div>
-    <h1>Die Todos von VD25</h1>
-    <ul>
+<div class="bg-blue-300 max-w-2xl mx-auto rounded-xl p-4 mt-4">
+    <h1 class="text-2xl font-bold underline mb-4">Die Todos von VD25</h1>
+    <table class="table table-fixed border border-2  border-gray-900 rounded text-sm">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Beschreibung</th>
+            <th>erld.?</th>
+            <th>erstellt</th>
+            <th>geändert</th>
+        </tr>
+        </thead>
+        <tbody>
         @foreach($todos as $todo)
-            <li>{{ $todo->description }}</li>
+            <tr class="text-sm border border-gray-900">
+                <td class="border border-gray-900 px-2 py-1">{{ $todo->id }}</td>
+                <td class="border border-gray-900 px-2 py-1">{{ $todo->description }}</td>
+                <td class="border border-gray-900 px-2 py-1">{{ $todo->done?'X':'' }}</td>
+                <td class="border border-gray-900 px-2 py-1">{{ $todo->created_at }}</td>
+                <td class="border border-gray-900 px-2 py-1">{{ $todo->updated_at }}</td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
+
 </div>
 
 </body>
